5e7d497c4d54f267947ae85e8b5adeec03fd55e6
AlekVT <alek.vantichelen@gmail.com>
2018-09-06 16:25:34 +0200

    add movies to redis db code

--- movies/movieservice/urls.py
+++ movies/movieservice/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'movieservice'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('actors', views.actors, name='actors'),
 ]
--- movies/movieservice/views.py
+++ movies/movieservice/views.py
@@ -5,18 +5,30 @@ import redis
 
 r = redis.StrictRedis()
 
-movielist = r.keys('movie:*')
+movielist = r.keys('film:*')
 if len(movielist) == 0:
 	movies = open("movies.txt", "r")
 	movielist = []
 	actorlist = []
 	for line in movies:
 		actorlist.append(line)
-		movielist.append('movie:' + line.split(" : ")[0])
+		movielist.append('film:' + line.split(" : ")[0])
+
+	for actor in actorlist:
+		r.sadd("film:" + actor.split(" : ")[0], actor.split(" : ")[1])
+
+print(movielist)
 
 def index(request):
     context = {
         'movielist': movielist,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'movieservice/index.html', context)
 
+def actors(request):
+	movie_name = request.POST['movie_name']
+	actors = "not found"
+	for actors in actorlist:
+		if actors.startswith(movie_name):
+			actors = movie_name.split(" : ")[1]
+	return HttpResponse(actors)
