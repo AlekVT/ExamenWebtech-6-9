0793a76f51f93992ee78f93cd5acc4c54b3b0299
AlekVT <alek.vantichelen@gmail.com>
2018-09-06 15:09:10 +0200

    function to read file

--- movies/movieservice/models.py
+++ movies/movieservice/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.answer_text
+
+class Actors(models.Model):
+	actors_text = models.CharField(max_length=200)
+	def __str__(self):
+		return self.answer_text
--- movies/movieservice/views.py
+++ movies/movieservice/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+import redis
+
+r = redis.StrictRedis()
+
+movielist = r.keys('movie:*')
+if len(movielist) == 0:
+	movies = open("movies.txt", "r")
+	movielist = []
+	actorlist = []
+	for line in movies:
+		actorlist.append(line)
+		movielist.append('movie:' + line.split(" : ")[0])
+
+def index(request):
+    context = {
+        'movielist': movielist,
+    }
+    return render(request, 'index.html', context)
+
